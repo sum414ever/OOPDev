bf3179ccd40bdd8c4332793bf2e111b0ed8fdc91
Copoka Alex <copokaalekc@gmai.com>
2018-07-22 18:11:37 +0300

    Add Junit Test Case

---
+++ OOPGit/Test/info/trapezoid/TrapezoidTest.java
@@ -0,0 +1,14 @@
+package info.trapezoid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrapezoidTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ OOPGit/Test/info/triangle/TriangleTest.java
@@ -0,0 +1,14 @@
+package info.triangle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
