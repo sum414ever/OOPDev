c4e4503e30a0ef1018e8bb247b10b5a416d27355
Copoka Alex <copokaalekc@gmai.com>
2018-07-23 20:24:41 +0300

    Make test for Circle

--- OOPGit/test/info/circle/CircleTest.java
+++ OOPGit/test/info/circle/CircleTest.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import info.sjd.circle.Circle;
+import javafx.geometry.Side;
+
 public class CircleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetArea() {
+		Circle circle  = new Circle();
+		circle.setRadius(10);
+		
+		assertNotNull(circle.getArea());
+		
+		assertTrue(circle.getArea() >300 && circle.getArea() < 400);
+		
+		assertEquals(314, circle.getArea());
+		
 	}
 
 }
